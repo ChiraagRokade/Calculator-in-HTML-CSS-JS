08711da4c6347d5b61cac07fcadaafa2d1ee78fe
Chiraag Sanjeev Rokade <94799807+ChiraagRokade@users.noreply.github.com>
2022-02-06 12:25:29 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+buttons.forEach(function (button) {
+  button.addEventListener('click', calculate);
+});
+function calculate (event) {
+  const clickButtonValue = event.target.value;
+  if (clickButtonValue === '=') {
+    if (display.value !== '') {
+      display.value = eval(display.value);
+    }
+  } else if (clickButtonValue === 'C') {
+    display.value = '';
+  } else {
+    display.value += clickButtonValue;
+  }
+}
